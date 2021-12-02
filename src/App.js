a771836e5c2ca781d5ed7545142fa10c035ca87a
aditReza <lete.note3@gmail.com>
2021-12-02 13:54:29 +0700

    first init app with webpack

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
-const App = () => "Hello World !!"
+import React from 'react';
+
+const App = () => (
+  <>
+    <h1>Hello World !!</h1>
+    <p>Enim pariatur est qui sit tempor ullamco esse eiusmod sint enim. Ex enim laboris nostrud eiusmod culpa aliquip dolore Lorem ullamco. Commodo dolor nostrud velit labore Lorem adipisicing aliqua dolor.</p>
+  </>
+)
 
 export default App;
